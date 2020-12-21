302f98acdf9381a8903fc45300b19016069f961d
Cody-Cao <codykhoa123@gmail.com>
2020-12-21 20:37:32 +0700

    Adding changes to API

--- project/js/main.js
+++ project/js/main.js
@@ -1,14 +1,23 @@
-$(document).ready(function(){
-  getWeather();
-})
+/*$(document).ready(function(){
+    getWeather();
+})*/
   
 
-function getWeather(){
-  var url="https://api.openweathermap.org/data/2.5/weather?q=Greencastle&APPID="+apiKey;
+function getWeather(searchQuery){
+  var url="https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&APPID="+apiKey;
 
+  $(".city").text("");
+  $(".temp").text("");
+  $(".error-message").text("");
   $.ajax(url,{success:function(data){
     $(".city").text(data.name);
     $(".temp").text(data.main.temp);
+  }, error: function(error){
+    $(".error-message").text("An error occured");
   }})
 }
   
+function searchWeather(){
+  var searchQuery = $(".search").val();
+  getWeather(searchQuery)
+}
